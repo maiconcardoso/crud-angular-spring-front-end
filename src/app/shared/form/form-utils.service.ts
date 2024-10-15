05d836ebd18d3638e79cc9a2249d126e93d63481
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-10-14 21:23:35 -0300

    Update v17

---
+++ src/app/shared/form/form-utils.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { UntypedFormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormUtilsService {
+
+  constructor() { }
+
+  getErrorMessage(formGroup: UntypedFormGroup ,fieldName: string) {
+    const field = formGroup.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'Campo Obrigatório'
+    }
+
+    if (field?.hasError('minLength')) {
+      const requiredLength: number = field.errors ? field.errors['minLength']['requiredLength'] : 5
+      return `Tamanho minímo precisa ser de ${requiredLength} caracteres.`;
+    }
+
+    if (field?.hasError('maxLength')) {
+      const requiredLength: number = field.errors ? field.errors['maxLength']['requiredLength'] : 80
+      return `Tamanho maxímo excedido de ${requiredLength} caracteres.`;
+    }
+
+    return 'Campo inválido.'
+  }
+}
